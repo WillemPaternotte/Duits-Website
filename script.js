018ae5c4c99105e7b9fcc7a20af70a4ae227c851
WillemPaternotte <51127114+WillemPaternotte@users.noreply.github.com>
2019-05-28 17:36:19 +0200

    wowo
    nonon

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 $(window).on('load', () => {
     // Hier doe je functies die runnen als de website geladen is (geen functie declaraties)
-
-    insertArticle({
-        "h1": "gert",
-        "h2": "wat heeft hij nu weer gedaan?!",
-        "p": "omg ik kan het niet geloven"
-    })
-
     insertArticleFromFile('artikelen.json')
 })
